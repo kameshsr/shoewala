111870969bc8e1befdb46f61a712cd2c5106d71d
kameshsr <kameshsr1338@gmail.com>
2026-01-13 14:24:13 +0530

    added home screen

--- app/src/main/java/com/shoewala/app/MainActivity.kt
+++ app/src/main/java/com/shoewala/app/MainActivity.kt
@@ -4,18 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.google.firebase.auth.FirebaseUser
-import com.shoewala.app.ui.theme.ShoewalaTheme
-import android.util.Log
 import com.shoewala.app.ui.auth.AuthScreen
 import com.shoewala.app.ui.home.HomeScreen
+import com.shoewala.app.ui.theme.ShoewalaTheme
 import com.shoewala.app.utils.FirebaseUtil
 
 class MainActivity : ComponentActivity() {
