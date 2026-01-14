5e9eaba082a2a77cbcc062941e5ec849cfde98b8
kameshsr <kameshsr1338@gmail.com>
2026-01-14 14:39:54 +0530

    added no product available screen

--- app/src/main/java/com/shoewala/app/ui/home/HomeScreen.kt
+++ app/src/main/java/com/shoewala/app/ui/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.shoewala.app.ui.home
 
 import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -60,8 +61,12 @@ fun HomeScreen() {
                     modifier = Modifier
                         .padding(12.dp)
                         .fillMaxWidth()
+                        .clickable {
+                            onProductClick(product)
+                        }
                 ) {
-                    Column {
+
+                Column {
                         AsyncImage(
                             model = product.imageUrl,
                             contentDescription = product.name,
